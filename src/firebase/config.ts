30c4b54d02c322b7f1f80f0e808c7ed52f31175e
Noah Aung <noah@advertomedia.co.uk>
2025-11-04 11:41:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6846103342-39d5b",
+  "appId": "1:323819567529:web:e822c92465435ba8cb6cb8",
+  "apiKey": "AIzaSyB1G10aOdc0_EpLQUb55HQiJWj-4a4AsJs",
+  "authDomain": "studio-6846103342-39d5b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "323819567529"
+};
